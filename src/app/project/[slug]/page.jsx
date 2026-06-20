c0b30e4f8f76f10b5ff3ab7ca61d573f5cf528c5
nayaknishant656 <nayaknishant656@gmail.com>
2026-06-20 23:11:24 +0530

    fix: will make the Slug Now

--- src/app/project/[slug]/page.jsx
+++ src/app/project/[slug]/page.jsx
@@ -4,7 +4,12 @@ function page() {
     return (
         <div className='grandparent-project'>
             <div className='parent-project'>
-
+                <div className='heading-project'>
+                    <h1>Emotionally-AI</h1>
+                </div>
+                <div className='introduction-project'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ipsa corrupti nesciunt qui consequuntur maxime accusamus sequi tenetur dolores delectus minima, eos illum optio voluptas possimus sunt exercitationem necessitatibus? Reiciendis impedit rerum in nihil voluptatum provident sit eligendi aut itaque id, autem optio eveniet blanditiis suscipit, voluptate error aliquid est.</div>
+                <div className='des-project'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, deserunt!</div>
+                <div className='img-project'></div>
             </div>
         </div>
     )
