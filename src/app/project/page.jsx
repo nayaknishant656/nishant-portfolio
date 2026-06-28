15774f8a98b869e3b0153ce373d25387d2f3a239
nayaknishant656 <nayaknishant656@gmail.com>
2026-06-28 22:16:02 +0530

    feat: developed the Listing page of the Project SLUG Blog

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+const data = {
+    project: [
+        {
+            id: 1,
+            title: "Emotionally-AI",
+            description: "i developed and i didnt want to build but anyways i tried and build these project",
+            link: "/project/1",
+        },
+    ],
+};
+
 function page() {
     return (
-        <div>Hello project this side of the Following </div>
+        <div>
+            {data.project.map((item) => (
+                <div key={item.id}>
+                    <h1>{item.title}</h1>
+                    <p>{item.description}</p>
+                    <a href={item.link}>View Project</a>
+                </div>
+            ))}
+        </div>
     )
 }
 
