0b186498e77999d75df80e975bc189bd5d2022c8
nayaknishant656 <nayaknishant656@gmail.com>
2026-06-28 22:00:04 +0530

    feat: Developed the Refined JSON data for the Blog SLUG page.jsx, Fixed the Project .json

--- src/app/project/[slug]/page.jsx
+++ src/app/project/[slug]/page.jsx
@@ -1,21 +1,59 @@
-import React from 'react'
+"use client";
+
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import "./page.css"
-// import Waterloo from './waterloo.png'
+import { useParams } from 'next/navigation'
+import projectData from '../project.json'
+
 function Page() {
+    const params = useParams();
+    const slug = params?.slug;
+
+    const { blogs } = projectData;
+    const [project, setProject] = useState(null);
+
+    useEffect(() => {
+        if (slug) {
+            const foundProject = blogs.find(
+                b => b.slug === slug ||
+                    b.title.toLowerCase() === slug.toLowerCase() ||
+                    b.title.toLowerCase().replace(/[^a-z0-9]+/g, '-') === slug.toLowerCase()
+            );
+            setProject(foundProject);
+        }
+    }, [slug, blogs]);
+
+    const { title, date, introduction, description, coverImage, content } = project || {};
+
     return (
         <div className='grandparent-project'>
             <div className='parent-project'>
                 <div className='heading-project'>
-                    <h1 className='heading-project'>Emotionally-AI</h1>
-                    <h3 className='text-2xl'>april 13 - 20 april</h3>
+                    <h1 className='heading-project'>{title}</h1>
+                    <h3 className='text-2xl'>{date}</h3>
                 </div>
-                <div className='introduction-project rem-top'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, ipsa corrupti nesciunt qui consequuntur maxime accusamus sequi tenetur dolores delectus minima, eos illum optio voluptas possimus sunt exercitationem necessitatibus? Reiciendis impedit rerum in nihil voluptatum provident sit eligendi aut itaque id, autem optio eveniet blanditiis suscipit, voluptate error aliquid est.</div>
-                <div className='des-project rem-top'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, deserunt!</div>
+                <div className='introduction-project rem-top'>{introduction}</div>
+                <div className='des-project rem-top'>{description}</div>
                 <div className='img-project'>
                 </div>
                 <div className='head_para_description'>
                     <div>
+                        {content?.map((item, i) => (
+                            <>
+                                <h1 className='rem-top'>{item.title}</h1>
+                                <Image
+                                    className='rem-top'
+                                    src={item.image}
+                                    alt="Picture of the author"
+                                    width={500}
+                                    height={500}
+                                />
+                                <p className='rem-top'>{item.description}</p>
+                            </>
+                        ))}
+                    </div>
+                    {/* <div>
                         <Image
                             className='rem-top'
                             src="/mitremedia.png"
@@ -23,9 +61,9 @@ function Page() {
                             width={500}
                             height={500}
                         />
-                        <p className='rem-top'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, odio corporis voluptates illo debitis, culpa voluptatum, quasi quibusdam ab dolorum ducimus atque excepturi esse adipisci distinctio nisi. Quae, iusto voluptatem!</p>
-                    </div>
-                    <div>
+                        <p className='rem-top'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, odio corporis voluptates illo debitis, culpa voluptatum, quasi quibusdam ab dolorum ducimus atque excepturi esse adipisci distinctio nisi. Quae, iusto voluptatem!</p>
+                    </div> */}
+                    {/* <div>
                         <Image
                             className='rem-top'
                             src="/mitremedia.png"
@@ -44,17 +82,7 @@ function Page() {
                             height={500}
                         />
                         <p className='rem-top'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, odio corporis voluptates illo debitis, culpa voluptatum, quasi quibusdam ab dolorum ducimus atque excepturi esse adipisci distinctio nisi. Quae, iusto voluptatem!</p>
-                    </div>
-                    <div>
-                        <Image
-                            className='rem-top'
-                            src="/mitremedia.png"
-                            alt="Picture of the author"
-                            width={500}
-                            height={500}
-                        />
-                        <p className='rem-top'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur, odio corporis voluptates illo debitis, culpa voluptatum, quasi quibusdam ab dolorum ducimus atque excepturi esse adipisci distinctio nisi. Quae, iusto voluptatem!</p>
-                    </div>
+                    </div> */}
 
                 </div>
             </div>
