8bd2a093402583dbb63e522cb78bbbb153faacbc
nayaknishant656 <nayaknishant656@gmail.com>
2026-07-24 14:41:13 +0530

    added: changed JSON data about the Portfolio and Profile , Manipulated few Information about the Profile

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,27 +1,61 @@
 import React from 'react'
-
+import BlurFade from "@/components/magicui/blur-fade";
 const data = {
     project: [
         {
             id: 1,
-            title: "Emotionally-AI",
-            description: "i developed and i didnt want to build but anyways i tried and build these project",
+            title: "Snappy",
+            description: "Snappy-Poornima Project",
             link: "/project/1",
+        }, {
+            id: 2,
+            title: "AI-Journal",
+            description: "AI-Journal Project",
+            link: "/project/2",
+        }, {
+            id: 3,
+            title: "Trading-Market-Place",
+            description: "Trading-Market-Place Project",
+            link: "/project/3",
+        }, {
+            id: 4,
+            title: "Paardarshi",
+            description: "Paardarshi Project",
+            link: "/project/4",
+        }, {
+            id: 5,
+            title: "Doctor-AI",
+            description: "Doctor-AI Platform tactics",
+            link: "/project/5",
+        },
+        {
+            id: 6,
+            title: "Nawnit-Studio-Prints",
+            description: "Nawnit-Studio-Prints",
+            link: "/project/6",
+        },
+        {
+            id: 7,
+            title: "React-Ecommerce",
+            description: "i developed and i didnt want to build but anyways i tried and build these project",
+            link: "/project/7",
         },
     ],
 };
-
+const BLUR_FADE_DELAY = 0.04;
 function page() {
     return (
-        <div>
-            {data.project.map((item) => (
-                <div key={item.id}>
-                    <h1>{item.title}</h1>
-                    <p>{item.description}</p>
-                    <a href={item.link}>View Project</a>
-                </div>
-            ))}
-        </div>
+        <BlurFade delay={BLUR_FADE_DELAY}>
+            <div>
+                {data.project.map((item) => (
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
+                        <p>{item.description}</p>
+                        <a href={item.link}>View Project</a>
+                    </div>
+                ))}
+            </div>
+        </BlurFade>
     )
 }
 
